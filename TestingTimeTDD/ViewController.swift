3d9d20d25f1ca8a08ff35fafd356d2188574555e
Paul Solt <PaulSolt@gmail.com>
2019-01-25 10:19:22 -0500

    Initial Commit

---
+++ TestingTimeTDD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingTimeTDD
+//
+//  Created by Paul Solt on 1/25/19.
+//  Copyright © 2019 Paul Solt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
