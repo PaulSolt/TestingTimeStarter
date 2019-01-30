27a3017d052b40c10e45e9981eabdc67d977a33d
Paul Solt <PaulSolt@gmail.com>
2019-01-30 14:43:03 -0500

    Refactored unit tests to use setUp() method. Added two passing tests

--- TestingTimeTDDTests/TrialPeriodTests.swift
+++ TestingTimeTDDTests/TrialPeriodTests.swift
@@ -14,32 +14,68 @@ import XCTest
 typealias DateGenerator = () -> Date
 
 class TrialPeriod {
+    static let defaultDuration = 7
+    
     var dateInstalled: Date
     var dateGenerator: DateGenerator
+    var durationInDays: Int
+    var dateExpired: Date {
+        return Calendar.current.date(byAdding: .day,
+                                     value: TrialPeriod.defaultDuration,
+                                     to: dateInstalled)!
+    }
+    
     
     init(dateGenerator: @escaping DateGenerator) {
         self.dateInstalled = dateGenerator()
         self.dateGenerator = dateGenerator
+        self.durationInDays = TrialPeriod.defaultDuration
     }
 }
 
 class TrialPeriodTests: XCTestCase {
 
+    var date: Date!
+    var timeTraveler: TimeTraveler!
+    var trial: TrialPeriod!
+
+    override func setUp() {
+        super.setUp()
+        
+        date = Date()
+        timeTraveler = TimeTraveler(date: date)
+        trial = TrialPeriod(dateGenerator: timeTraveler.generateDate)
+    }
+    
     // All test functions start with the word "test"
 
-    func testTrialPeriod() {
-        // Arrange
-        let expected = Date()
-        let timeTraveler = TimeTraveler(date: expected)
-        let trial = TrialPeriod(dateGenerator: timeTraveler.generateDate)
-        
-        // Act
-        let actualDate = trial.dateInstalled
-        
-        print("Expected: \(expected.timeIntervalSinceReferenceDate)")
-        print("Actual: \(actualDate.timeIntervalSinceReferenceDate)")
-        
-        // Assert
-        XCTAssertEqual(expected, actualDate)
+//    func testTrialPeriod() {
+//        // Arrange
+//        let expected = Date()
+//        let timeTraveler = TimeTraveler(date: expected)
+//        let trial = TrialPeriod(dateGenerator: timeTraveler.generateDate)
+//
+//        // Act
+//        let actualDate = trial.dateInstalled
+//
+//        print("Expected: \(expected.timeIntervalSinceReferenceDate)")
+//        print("Actual: \(actualDate.timeIntervalSinceReferenceDate)")
+//
+//        // Assert
+//        XCTAssertEqual(expected, actualDate)
+//    }
+    
+    func testTrialPeriodDateInstalledIsToday() {
+        XCTAssertEqual(date, trial.dateInstalled)
     }
+
+    func testDefaultDurationIs7Days() {
+        XCTAssertEqual(7, trial.durationInDays)
+    }
+    
+    func testDateExpiredIs7DaysAfterInstallDate() {
+        let expected = Calendar.current.date(byAdding: .day, value: 7, to: date)
+        XCTAssertEqual(expected, trial.dateExpired)
+    }
+    
 }
